3aa27acb78e133ba8613cc7ed6b3cd20e439e0e6
keen-coder <kknaur@gmail.com>
2022-03-18 09:57:08 -0700

    Updated linked list examples

--- Week08_LinkedLists/src/section05_06/linked_list/LinkedList.java
+++ Week08_LinkedLists/src/section05_06/linked_list/LinkedList.java
@@ -12,7 +12,66 @@ public class LinkedList<E> {
 	}
 	
 	public LinkedList(E ... values) {
-		//Fill out later.
+		for (E value : values) {
+			this.addLast(value);
+		}
+	}
+	
+	public void insert(int index, E value) {
+		if (index < 0 || index > this.size) {
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		
+		if (index == 0) {
+			this.addFirst(value);
+		}
+		else if (index == this.size) {
+			this.addLast(value);
+		}
+		else {
+			Node<E> temp = new Node<>(value);
+			Node<E> current = this.head;
+			int currentIndex = 0;
+			
+			while(currentIndex != (index - 1)) {
+				current = current.next;
+				currentIndex++;
+			}
+			
+			temp.next = current.next;
+			current.next = temp;
+			this.size++;
+		}
+	}
+	
+	public E get(int index) throws IndexOutOfBoundsException {
+		if (index < 0 || index >= this.size) {
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		
+		Node<E> current = this.head;
+		int currentIndex = 0;
+		
+		while(currentIndex != index) {
+			current = current.next;
+			currentIndex++;
+		}
+		
+		return current.data;
+	}
+	
+	public void addLast(E data) {
+		Node<E> temp = new Node<>(data);
+		
+		if (this.isEmpty()) {
+			this.head = this.tail = temp;
+		}
+		else {
+			this.tail.next = temp;
+			this.tail = temp;
+		}
+		
+		this.size++;
 	}
 	
 	public void addFirst(E data) {
--- Week08_LinkedLists/src/section05_06/tester/LinkedListTester.java
+++ Week08_LinkedLists/src/section05_06/tester/LinkedListTester.java
@@ -6,11 +6,65 @@ public class LinkedListTester {
 	
 	public static void main(String[] args) {
 		testAddFirst();
+		System.out.println();
+		testAddLast();
+		System.out.println();
+		testGet();
+		System.out.println();
 		
+		LinkedList<Integer> list = new LinkedList<>(10, 20, 30, 40, 50);
+		list.insert(1, 15);
+		list.insert(3, 25);
+		list.insert(5, 35);
+		list.insert(7, 45);
 		
+		System.out.println(list);
+	}
+	
+	public static void testGet() {
+		System.out.println("Testing get()------------------");
+		
+		LinkedList<Integer> list = new LinkedList<>(10, 20, 30, 40, 50);
+		
+		int[] expectedValues = {10, 20, 30, 40, 50};
+		
+		for (int i = 0 ; i < expectedValues.length ; i++) {
+			int expected = expectedValues[i];
+			int actual = list.get(i);
+			boolean isCorrect = (expected == actual);
+			
+			System.out.printf("%-15s %d\n" ,"Expected:", expected);
+			System.out.printf("%-15s %d\n" ,"Actual:", actual);
+			System.out.printf("%-15s %b\n" ,"isCorrect?:", isCorrect);
+			System.out.println();
+		}
 		
 		
 
+		System.out.println("End Testing get()--------------------");
+	
+	}
+	
+	public static void testAddLast() {
+		System.out.println("Testing Last First------------------");
+		
+		LinkedList<Integer> list = new LinkedList<>();
+		
+		list.addLast(10);
+		list.addLast(20);
+		list.addLast(30);
+		list.addLast(40);
+		list.addLast(50);
+		
+		String expected = "10, 20, 30, 40, 50";
+		String actual = list.toString();
+		boolean isCorrect = expected.equals(actual);
+		
+		System.out.printf("%-15s %s\n" ,"Expected:", expected);
+		System.out.printf("%-15s %s\n" ,"Actual:", actual);
+		System.out.printf("%-15s %b\n" ,"isCorrect?:", isCorrect);
+
+		System.out.println("End Testing Add Last--------------------");
 	}
 	
 	public static void testAddFirst() {
